87f1780e372501c51aad9867c0087d70c2af8beb
Jhalak Gupta <gupta.j@husky.neu.edu>
2018-09-27 03:50:30 -0400

    Changes done to take the request in JSON format

--- webapp/login/src/main/java/com/neu/controller/LoginController.java
+++ webapp/login/src/main/java/com/neu/controller/LoginController.java
@@ -2,9 +2,7 @@ package com.neu.controller;
 
 import java.util.Base64;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.mindrot.jbcrypt.BCrypt;
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
@@ -14,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.time.LocalDateTime;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.json.JSONObject;
 
 @RestController
 public class LoginController implements CommandLineRunner{
@@ -31,42 +30,45 @@ public class LoginController implements CommandLineRunner{
             return new ResponseEntity<>("Enter basic authentication header! You are not logged in !! ", HttpStatus.OK);
 
         }else if(authHeader.startsWith("Basic")){
-        // Authorization: Basic base64credentials
-        String base64Credentials = authHeader.substring("Basic".length()).trim();
-        byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
-        String credentials = new String(credDecoded, StandardCharsets.UTF_8);
-        // credentials = username:password
-        final String[] values = credentials.split(":", 2);
-        String username = values[0];
-        String pass= values[1];
-        if(checkIfUserExists(username)){
-            String sql = "SELECT password FROM usertable WHERE username = ?";
-            String encryptedPassword = (String)jdbcTemplate.queryForObject(
-                    sql, new Object[] { username }, String.class);
-            if(encryptedPassword != null) {
-                if (!checkPassword(pass, encryptedPassword)) {
-                    //return "UserName and Password does not match !";
-                    return new ResponseEntity<>("UserName and Password does not match !", HttpStatus.OK);
+            // Authorization: Basic base64credentials
+            String base64Credentials = authHeader.substring("Basic".length()).trim();
+            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+            String credentials = new String(credDecoded, StandardCharsets.UTF_8);
+            // credentials = username:password
+            final String[] values = credentials.split(":", 2);
+            String username = values[0];
+            String pass= values[1];
+            if(checkIfUserExists(username)){
+                String sql = "SELECT password FROM usertable WHERE username = ?";
+                String encryptedPassword = (String)jdbcTemplate.queryForObject(
+                        sql, new Object[] { username }, String.class);
+                if(encryptedPassword != null) {
+                    if (!checkPassword(pass, encryptedPassword)) {
+                        //return "UserName and Password does not match !";
+                        return new ResponseEntity<>("UserName and Password does not match !", HttpStatus.OK);
 
+                    }
                 }
+            }else{
+                //return "User not registered!";
+                return new ResponseEntity<>("User not registered! ", HttpStatus.OK);
             }
-        }else{
-            //return "User not registered!";
-            return new ResponseEntity<>("User not registered! ", HttpStatus.OK);
+
+
         }
 
 
-    }
-
-
-       // return "Current Time in milliseconds = "+System.currentTimeMillis();LocalDateTime.now();
+        // return "Current Time in milliseconds = "+System.currentTimeMillis();LocalDateTime.now();
         return new ResponseEntity<>(LocalDateTime.now(), HttpStatus.OK);
 
     }
 
-    @RequestMapping("/user/register")
-    public String registerUser(@RequestParam(value="name") String name,@RequestParam(value="password") String password)
+    @RequestMapping(value = "/user/register", method = RequestMethod.POST)
+    public String registerUser(@RequestBody String jos)
     {
+        JSONObject jo = new JSONObject(jos);
+        String name = (String) jo.get("name");
+        String password = (String) jo.get("password");
         String encryptedPass = hashPassword(password);
         try {
             String[] s = new String[2];
@@ -74,6 +76,7 @@ public class LoginController implements CommandLineRunner{
             s[1]=encryptedPass;
             if(checkIfUserExists(name)){
                 return name+" "+ "This User is already registered";
+
             }else{
                 run(s);
             }
@@ -83,6 +86,7 @@ public class LoginController implements CommandLineRunner{
         }
 
         return name+" "+ encryptedPass;
+
 
     }
 
