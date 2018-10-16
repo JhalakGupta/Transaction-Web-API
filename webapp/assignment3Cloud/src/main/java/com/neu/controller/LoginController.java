06f32ac7f6b695e7f6dbf265e840af9f63134221
Varun Murthy <vmurthy@6river.com>
2018-10-16 09:23:04 -0400

    fix: Minor updates

--- webapp/assignment3Cloud/src/main/java/com/neu/controller/LoginController.java
+++ webapp/assignment3Cloud/src/main/java/com/neu/controller/LoginController.java
@@ -160,7 +160,6 @@ public class LoginController implements CommandLineRunner{
 
            } catch (Exception e) {
                e.printStackTrace();
-
            }
 
            return name + " " + encryptedPass;
@@ -193,25 +192,10 @@ public class LoginController implements CommandLineRunner{
            }else{
                run(s);
            }
-
-           return name + " " + encryptedPass;
-
+       } catch (Exception e) {
+           e.printStackTrace();
        }
-       return "Username not valid";
+       return name+" "+ encryptedPass;
    }
-
-    public static boolean isValid(String email)
-    {
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
-                "[a-zA-Z0-9_+&*-]+)*@" +
-                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                "A-Z]{2,7}$";
-
-        Pattern pat = Pattern.compile(emailRegex);
-        if (email == null) {
-            return false;
-        }
-        return pat.matcher(email).matches();
-    }
 
 }
