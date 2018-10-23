7022a392319bc83a9366c36b48f95dad30a3c65f
Jhalak Gupta <gupta.j@husky.neu.edu>
2018-10-22 22:58:47 -0400

    removed existing webapps

--- webapp/assignment3Cloud/src/main/java/com/neu/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.neu.repository;
-
-import com.neu.pojo.User;
-import com.neu.pojo.UserDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserDetails, String> {
-}
