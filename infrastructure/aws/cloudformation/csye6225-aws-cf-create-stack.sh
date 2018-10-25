01577ef0f801b36b503633a1cf3ca73c8fecbeba
Varun Murthy <murthy.va@husky.neu.edu>
2018-10-24 21:17:07 -0400

    Updated infrastructure and added code deploy scripts

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -8,9 +8,8 @@ SUBNET5_NAME=${STACK_NAME}-csye6225-subnet5
 SUBNET6_NAME=${STACK_NAME}-csye6225-subnet6
 IG_NAME=${STACK_NAME}-csye6225-InternetGateway
 PUBLIC_ROUTE_TABLE=${STACK_NAME}-csye6225-public-route-table
-PRIVATE_ROUTE_TABLE=${STACK_NAME}-csye6225-private-route-table
 
-aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-networking.json --parameters ParameterKey=VPCName,ParameterValue=$VPC_NAME ParameterKey=SubnetName1,ParameterValue=$SUBNET1_NAME ParameterKey=SubnetName2,ParameterValue=$SUBNET2_NAME ParameterKey=SubnetName3,ParameterValue=$SUBNET3_NAME ParameterKey=SubnetName4,ParameterValue=$SUBNET4_NAME ParameterKey=SubnetName5,ParameterValue=$SUBNET5_NAME ParameterKey=SubnetName6,ParameterValue=$SUBNET6_NAME ParameterKey=IGName,ParameterValue=$IG_NAME ParameterKey=PublicRouteTableName,ParameterValue=$PUBLIC_ROUTE_TABLE ParameterKey=PrivateRouteTableName,ParameterValue=$PRIVATE_ROUTE_TABLE
+aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-networking.json --parameters ParameterKey=VPCName,ParameterValue=$VPC_NAME ParameterKey=SubnetName1,ParameterValue=$SUBNET1_NAME ParameterKey=SubnetName2,ParameterValue=$SUBNET2_NAME ParameterKey=SubnetName3,ParameterValue=$SUBNET3_NAME ParameterKey=SubnetName4,ParameterValue=$SUBNET4_NAME ParameterKey=SubnetName5,ParameterValue=$SUBNET5_NAME ParameterKey=SubnetName6,ParameterValue=$SUBNET6_NAME ParameterKey=IGName,ParameterValue=$IG_NAME ParameterKey=PubicRouteTableName,ParameterValue=$PUBLIC_ROUTE_TABLE
 
 aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
 
