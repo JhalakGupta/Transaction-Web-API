f42dbc917b303b9f736efb3e993df2a5ab2456bc
Jhalak Gupta <gupta.j@husky.neu.edu>
2018-10-29 03:02:56 -0400

    TestRun

--- webapp/src/main/java/com/neu/controller/UploadAttachmentS3BucketController.java
+++ webapp/src/main/java/com/neu/controller/UploadAttachmentS3BucketController.java
@@ -36,17 +36,17 @@ public class UploadAttachmentS3BucketController {
                // .withCredentials(new InstanceProfileCredentialsProvider(false))
                // .build();
 
-        /*AWSCredentials credentials = new ProfileCredentialsProvider().getCredentials();
+        AWSCredentials credentials = new ProfileCredentialsProvider().getCredentials();
 
         AmazonS3 s3Client = AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 //.withRegion(Regions.US_EAST_2)
-                .build();*/
+                .build();
 
-        InstanceProfileCredentialsProvider provider = new InstanceProfileCredentialsProvider(true);
+       /* InstanceProfileCredentialsProvider provider = new InstanceProfileCredentialsProvider(true);
 
-        AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(provider).withRegion(Regions.US_EAST_1).build();
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(provider).withRegion(Regions.US_EAST_1).build();*/
 
         String bucketName=null;
 
