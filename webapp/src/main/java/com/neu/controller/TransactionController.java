1a0dbecb37965de6090abd52b9470449dadc0efe
Varun Murthy <murthy.va@husky.neu.edu>
2018-11-14 21:31:42 -0500

    Update TransactionController.java

--- webapp/src/main/java/com/neu/controller/TransactionController.java
+++ webapp/src/main/java/com/neu/controller/TransactionController.java
@@ -833,7 +833,7 @@ public class TransactionController {
 
             for(Topic topic : topics){
 
-                if(topic.getTopicArn().endsWith("Noti")){
+                if(topic.getTopicArn().endsWith("ForgotPassword")){
                     PublishRequest req = new PublishRequest(topic.getTopicArn(),details.getUsername());
                     snsClient.publish(req);
                     break;
